7bf3274b2e911a7258709b943a062a8c25bc95b1
ItsSpres <95886944+ItsSpres@users.noreply.github.com>
2021-12-09 14:02:41 -0800

    Add files via upload

---
+++ HtmlToTextExt.swift
@@ -0,0 +1,23 @@
+//
+//  HtmlToTextExt.swift
+//  HtmlToText
+//
+//  Created by Ian Spresney on 12/9/21.
+//
+
+import Foundation
+import UIKit
+
+extension String {
+    var htmlToAttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return nil }
+        do {
+            return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch {
+            return nil
+        }
+    }
+    var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+    }
+}
